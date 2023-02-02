e75ecb66fdf91713a6a3b72ff4f7885bfaf81b12
adriano328 <lucasadrianodias@gmail.com>
2023-02-01 20:48:17 -0400

    ajuste

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
-  {
-    path: '', redirectTo: 'calendario', pathMatch: 'full'
-  },
-  {
-    path: 'calendario', loadChildren: ()=> import('./modules/calendar/calendar.module').then(m => m.CalendarModule)
-  }
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,72 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calendar-costumer';
+
+  monthsBR: any;
+  now = new Date();
+  mes = this.now.getMonth();
+  ano = this.now.getFullYear();
+
+  getDaysCalendar(mes: number, ano: number) {
+    document.getElementById('mes')!.innerHTML = this.monthsBR[mes];
+    document.getElementById('ano')!.innerHTML = String(ano);
+
+    const tableDays = document.getElementById('dias')!;
+
+    let firstDayOfWeek = new Date(ano, mes, 1).getDay() - 1;
+
+    let getLastDayThisMonth = new Date(ano, mes + 1, 0).getDate();
+
+    for (var i = -firstDayOfWeek, index = 0; i < (42 - firstDayOfWeek); i++, index++) {
+      let dt = new Date(ano, mes, i);
+      let dtNow = new Date();
+      var dayTable = tableDays!.getElementsByTagName('td')[index];
+      if (dayTable) {
+        dayTable.innerHTML = dt.getDate()!.toString()!;
+        dayTable.classList.remove('mes-anterior');
+        dayTable.classList.remove('proximo-mes');
+        dayTable.classList.remove('dia-atual');
+        if (dt.getFullYear() == dtNow.getFullYear() && dt.getMonth() == dtNow.getMonth() && dt.getDate() == dtNow.getDate()) {
+          dayTable!.classList.add('dia-atual')
+        }
+        if (i < 1) {
+          dayTable!.classList.add('mes-anterior')
+        }
+
+        if (i > getLastDayThisMonth) {
+          dayTable!.classList.add('proximo-mes')
+        }
+      }
+    }
+  }
+
+  constructor() {
+    this.monthsBR = ['JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO',
+      'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO'];
+  }
+
+  ngOnInit(): void {
+    let now = new Date();
+    let mes = now.getMonth();
+    let ano = now.getFullYear();
+    this.getDaysCalendar(mes, ano);
+  }
+
+  proximoMes() {
+    this.mes++;
+    if (this.mes > 11) {
+      this.mes = 0;
+      this.ano++;
+    }
+    this.getDaysCalendar(this.mes, this.ano)
+  }
+
+  mesAnterior() {
+    this.mes--;
+    if (this.mes < 0) {
+      this.mes = 11;
+      this.ano--;
+    }
+    this.getDaysCalendar(this.mes, this.ano)
+  }
 }
