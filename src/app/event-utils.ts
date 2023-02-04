10aa5690c172bac6f7f8cc8d45d0bd4e9a0a1d37
adriano328 <lucasadrianodias@gmail.com>
2023-02-04 14:20:08 -0400

    listagem eventos

---
+++ src/app/event-utils.ts
@@ -0,0 +1,31 @@
+import { EventInput } from '@fullcalendar/core';
+
+let eventGuid = 0;
+const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of today
+const END_EVENT = new Date("2023-02-06").toISOString().replace(/T.*$/, '');
+
+console.log(END_EVENT);
+
+export const INITIAL_EVENTS: EventInput[] = [
+  {
+    id: createEventId(),
+    title: 'All-day event',
+    start: TODAY_STR
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: TODAY_STR + 'T00:00:00',
+    end: TODAY_STR + 'T03:00:00'
+  },
+  {
+    id: createEventId(),
+    title: 'Timed event',
+    start: TODAY_STR + 'T12:00:00',
+    end: END_EVENT + 'T15:00:00'
+  }
+];
+
+export function createEventId() {
+  return String(eventGuid++);
+}
