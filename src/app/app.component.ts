0ddbe664166fbd8e2fefe176255959d3adb93fee
TR039070 <TR039070@minhati.com.br>
2023-02-05 12:05:42 -0400

    Alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,15 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
 import * as moment from 'moment';
 import { CalendarService } from './services/calendar.service';
 import { IEventos } from './shared/interfaces/IEventos';
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import { INITIAL_EVENTS, createEventId } from './event-utils';
+import momentPlugin from '@fullcalendar/moment';
+import { DayHeader } from '@fullcalendar/core/internal';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +17,97 @@ import { IEventos } from './shared/interfaces/IEventos';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  calendarVisible = true;
+
+  calendarOptions: CalendarOptions = {
+    // titleFormat: {
+    //   weekday: 'long'
+    // },
+    // dayGridMonth: {
+    //   dayHeaderFormat: {
+    //     weekday: 'long'
+    //   }
+    // }
+
+    dayHeaderFormat: {
+      weekday: 'long'
+    },
+    locale: "pt-br",
+    // views: {
+    //   month: {
+    //     columnFormat: 'dddd' // set format for month here
+    //   },
+
+    //   week: {
+    //     columnFormat: 'ddd d/M' // set format for week here
+    //   },
+    //   day: {
+    //     columnFormat: 'ddd Do' // set format for day here
+    //   }
+    // },
+    plugins: [
+      interactionPlugin,
+      dayGridPlugin,
+      timeGridPlugin,
+      listPlugin,
+      momentPlugin
+    ],
+    // titleFormat: 'dddd, MMMM D, YYYY',
+    // headerToolbar: {
+    //   left: 'prev,next today',
+    //   center: 'title',
+    //   right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    // },
+    initialView: 'dayGridMonth',
+    initialEvents: INITIAL_EVENTS, // alternatively, use the `events` setting to fetch from a feed
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
+    eventClick: this.handleEventClick.bind(this),
+    eventsSet: this.handleEvents.bind(this),
+    /* you can update a remote database when these fire:
+    eventAdd:
+    eventChange:
+    eventRemove:
+    */
+  };
+
+  constructor(private changeDetector: ChangeDetectorRef) {
+  }
+
+  handleDateSelect(selectInfo: DateSelectArg) {
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      });
+    }
+  }
+
+  handleEventClick(clickInfo: EventClickArg) {
+    if (confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove();
+    }
+  }
+
+  handleEvents(events: EventApi[]) {
+    this.currentEvents = events;
+    this.changeDetector.detectChanges();
+  }
+
+  currentEvents: EventApi[] = [];
+
   ngOnInit(): void {
   }
 }
