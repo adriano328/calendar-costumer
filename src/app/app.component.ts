d3245c102949df2b7b41f32f21cb9014d6266d59
TR039070 <TR039070@minhati.com.br>
2023-02-05 18:45:33 -0400

    Alterações

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -96,6 +96,58 @@ export class AppComponent {
   async listAllEventsByMonths() {
   }
 
+  test(){
+    let month;
 
+    if(document.getElementsByClassName('fc-toolbar-title')){
+      month = document.getElementsByClassName('fc-toolbar-title')[0].textContent;
+    }
+
+    if(month){
+      month = this.convertMonthNameToNumberOfMonth(month);
+    }
+
+
+    const date = new Date(`${new Date().getFullYear()}-${month}-01T06:00:00Z`);
+    let firstDay = new Date( date.getFullYear() ,date.getMonth(), 1).getDate()
+    let lastDay = new Date( date.getFullYear() ,date.getMonth() + 1, 0).getDate()
+    console.log(firstDay, lastDay);
+
+
+
+// const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+// console.log(lastDay);
+
+  }
+
+  convertMonthNameToNumberOfMonth(month: string): string {
+    if(month.includes('janeiro')){
+      return '01';
+    } else if(month.includes('fevereiro')){
+      return '02';
+    } else if(month.includes('março')){
+      return '03';
+    } else if(month.includes('abril')){
+      return '04';
+    } else if(month.includes('maio')){
+      return '05';
+    } else if(month.includes('junho')){
+      return '06';
+    } else if(month.includes('julho')){
+      return '07';
+    } else if(month.includes('agosto')){
+      return '08';
+    } else if(month.includes('setembro')){
+      return '09';
+    } else if(month.includes('outubro')){
+      return '10';
+    } else if(month.includes('novembro')){
+      return '11';
+    } else if(month.includes('dezembro')){
+      return '12';
+    }
+
+    return '00';
+  }
 
 }
