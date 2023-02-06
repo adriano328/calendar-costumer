9662b53403763b927e2487f29795bb2aa2016042
adriano328 <lucasadrianodias@gmail.com>
2023-02-06 13:02:59 -0400

    implementação de css

--- src/app/event-utils.ts
+++ src/app/event-utils.ts
@@ -5,24 +5,24 @@ const TODAY_STR = new Date().toISOString().replace(/T.*$/, ''); // YYYY-MM-DD of
 const END_EVENT = new Date("2023-02-06").toISOString().replace(/T.*$/, '');
 export const INITIAL_EVENTS: EventInput[] = [
 
-  // {
-  //   id: createEventId(),
-  //   title: 'Alefe',
-  //   start: TODAY_STR + 'T13:00:00',
-  //   end: END_EVENT + 'T15:00:00'
-  // },
-  // {
-  //   id: createEventId(),
-  //   title: 'Alefe',
-  //   start: TODAY_STR + 'T12:00:00',
-  //   end: END_EVENT + 'T15:00:00'
-  // },
-  // {
-  //   id: createEventId(),
-  //   title: 'Alefe',
-  //   start: TODAY_STR + 'T12:00:00',
-  //   end: END_EVENT + 'T15:00:00'
-  // }
+  {
+    id: createEventId(),
+    title: 'Alefe',
+    start: TODAY_STR + 'T13:00:00',
+    end: END_EVENT + 'T15:00:00'
+  },
+  {
+    id: createEventId(),
+    title: 'Alefe',
+    start: TODAY_STR + 'T12:00:00',
+    end: END_EVENT + 'T15:00:00'
+  },
+  {
+    id: createEventId(),
+    title: 'Alefe',
+    start: TODAY_STR + 'T12:00:00',
+    end: END_EVENT + 'T15:00:00'
+  }
 ];
 
 export function createEventId() {
