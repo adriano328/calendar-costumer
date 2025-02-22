d2a4bd2495b548ee5f99df49416d83c5e81a05f2
adriano328 <lucasadrianodias@gmail.com>
2025-02-22 13:56:37 -0400

    listAllEventsByMonths

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   // url: 'http://82.180.163.22:1024',
-   url:'http://172.23.153.226:8080',
-  // url: 'https://api-portal.ieadcuiaba.com',
+  //  url:'http://172.23.153.226:8080',
+  url: 'https://api-portal.ieadcuiaba.com',
   // url: 'https://api-test-portal.ieadcuiaba.com',
   production: false
 };
